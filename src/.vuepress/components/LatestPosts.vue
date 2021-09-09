638c6aa9f74004b157f199b8c843e83cb9edfd83
naoyamaki <stringbassandsticks@gmail.com>
2021-09-09 23:28:55 +0900

    SPのデザイン調整

--- src/.vuepress/components/LatestPosts.vue
+++ src/.vuepress/components/LatestPosts.vue
@@ -17,50 +17,61 @@
 </template>
 
 <style lang="scss" scoped>
-@media (min-width: 720px) {
-  article.card {
-    margin: 16px 0;
-    border: 1px solid #9e9e9e;
-    border-radius: 16px;
-    box-shadow: 2px 2px 2px 0 rgba(0, 0, 0, .5);
-    a {
-      display: flex;
-      figure {
-        flex: 2;
-        margin: 1%;
-        position: relative;
-        img {
-          border-radius: 16px;
-        }
-        span {
-          position: absolute;
-          top: .5em;
-          left: .5em;
-          background:rgba(255, 255, 255, 0.6);
-          border-radius: 16px;
-          padding: .2em;
-        }
+article.card {
+  margin: 16px 0;
+  border: 1px solid #9e9e9e;
+  border-radius: 16px;
+  box-shadow: 2px 2px 2px 0 rgba(0, 0, 0, .5);
+  a {
+    display: flex;
+    figure {
+      flex: 2;
+      margin: 1%;
+      position: relative;
+      img {
+        border-radius: 16px;
       }
-
-      div.sentence {
-        min-height: 192px;
-        flex: 3;
-        display: flex;
-        flex-direction: column;
-        p.title {
-          font-size: 1.5em;
-          margin: .5em 0;
-        }
-        p.description {
-          font-size: 1em;
-          margin: .5em 0;
-        }
-        time {
-          text-align: right;
-          margin: .5em;
-        }
+      span {
+        position: absolute;
+        top: .5em;
+        left: .5em;
+        background:rgba(255, 255, 255, 0.6);
+        border-radius: 16px;
+        padding: .2em;
       }
     }
+
+    div.sentence {
+      min-height: 192px;
+      flex: 3;
+      display: flex;
+      flex-direction: column;
+      p.title {
+        font-size: 1.5em;
+        margin: .5em 0;
+      }
+      p.description {
+        font-size: 1em;
+        margin: .5em 0;
+      }
+      time {
+        text-align: right;
+        margin: .5em;
+      }
+    }
+  }
+}
+@media (max-width:720px) {
+  article.card {
+  a {
+    flex-direction: column;
+    figure {
+      margin: 0;
+      img {
+        border-radius: 16px 16px 0 0;
+      }
+    }
+  }
   }
 }
 
